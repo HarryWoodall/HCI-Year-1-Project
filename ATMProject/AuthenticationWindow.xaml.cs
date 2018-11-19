29a8183adef825be0d95f04ebc5b8fc964210331
Harry <h.woodall311@hotmail.co.uk>
2018-11-19 10:07:38 +0000

    Added stuff for workshop

---
+++ ATMProject/AuthenticationWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ATMProject {
+    /// <summary>
+    /// Interaction logic for AuthenticationWindow.xaml
+    /// </summary>
+    public partial class AuthenticationWindow : Window {
+
+        private String input = "";
+
+        public AuthenticationWindow() {
+            InitializeComponent();
+            buttonBorder.Visibility = Visibility.Visible;
+            authenticator.Visibility = Visibility.Hidden;
+        }
+
+        private void InsertTokenPress(object sender, MouseButtonEventArgs e) {
+            buttonBorder.Visibility = Visibility.Hidden;
+            authenticator.Visibility = Visibility.Visible;
+        }
+
+        private void numericButtonPress(object sender, MouseButtonEventArgs e) {
+            Label label = (Label)sender;
+            UITimers timer = new UITimers();
+            timer.colorTimer(label, new SolidColorBrush(Color.FromArgb(255, 78, 123, 220)), 5);
+
+            if (input.Length < 4) {
+                input += label.Content;
+                authenticatorOutput.Text += "•";
+
+                if (input.Length == 4) {
+                    confirmButton.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x4B, 0xC9, 0x37));
+                    confirmButton.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF));
+                }
+            } else {
+                SoundPlayer player = new SoundPlayer("../../Assests/Sounds/Error_Select.wav");
+                player.Load();
+                player.Play();
+            }
+        }
+
+        private void confirmButtonPress(object sender, MouseButtonEventArgs e) {
+            if (input.Length == 4) {
+                string[] token = new string[3];
+                try {
+                    using (StreamReader sr = new StreamReader("../../Assests/Token.txt")) {
+                        for (int i = 0; i < token.Length; i++) {
+                            token[i] = sr.ReadLine();
+                        }
+                    }
+                } catch (Exception ex) {
+                    Console.WriteLine("Error");
+                    Console.WriteLine(ex.Message);
+                }
+
+                if (token[1] == input) {
+                    Console.WriteLine("Authenticated");
+                } else {
+                    Console.WriteLine("Invalid Pin");
+                }
+            }
+        }
+
+        private void cancelButtonPress(object sender, MouseButtonEventArgs e) {
+            authenticatorOutput.Text = "";
+            input = "";
+            confirmButton.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xB6, 0xB6, 0xB6));
+            confirmButton.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xC9, 0xC9, 0xC9));
+        }
+    }
+}
---
+++ ATMProject/UITimers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ATMProject {
+
+    class UITimers {
+
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int timerTickCount;
+        private int timerTickTarget;
+        private Control control;
+
+        private SolidColorBrush targetColor;
+        private SolidColorBrush naturalColor;
+
+
+        public UITimers() {
+            timer.Tick += colorTimerTick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+        }
+
+        private void colorTimerTick(object sender, EventArgs e) {
+            if (timerTickCount >= 2 && control.Background == naturalColor) {
+                timerTickCount = 0;
+                control.Background = targetColor;
+            } else {
+                timerTickCount++;
+            }
+
+            if (timerTickCount >= timerTickTarget && control.Background == targetColor) {
+                timer.Stop();
+                timerTickCount = 0;
+                control.Background = naturalColor;
+            } else {
+                timerTickCount++;
+            }
+        }
+
+
+        public void colorTimer(Control control, SolidColorBrush brush, int time) {
+            this.control = control;
+            naturalColor = (SolidColorBrush)control.Background;
+            targetColor = brush;
+            timerTickTarget = time;
+            timer.Start();
+        }
+    }
+}
